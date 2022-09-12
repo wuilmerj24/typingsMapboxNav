64331745955c6f1c8487e4569cc85914ade48d94
wuilmerj24 <wilmermorgado24@gmail.com>
2022-09-12 10:41:02 -0400

    first commit

---
+++ references.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
+/// <reference path="./typings/android/directions.model.refresh.d.ts" />
+/// <reference path="./typings/android/gestures.mapbox.ts" />
+/// <reference path="./typings/android/location.component.ts" />
